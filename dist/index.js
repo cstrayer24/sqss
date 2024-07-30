a55580de2b10dd9cf056ade75311d5eab975c509
cstrayer24 <cstrayer4303@gmail.com>
2024-07-29 23:39:54 -0400

    error stuff

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var transpileFile_1 = require("./transpileFile");
 var fs_1 = require("fs");
+function printErrStyle(errStr) {
+    console.log("\u001B[1m\u001B[31m".concat(errStr));
+}
+printErrStyle("hi");
 var args = process.argv.slice(2);
 var filePath = args[0];
 //test for a -o flag and set the output path to the command line argument directly adjacant to it otherwise default to style.css
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import transpileFile from "./transpileFile";
 import { writeFileSync } from "fs";
+function printErrStyle(errStr: string) {
+  console.log(`\x1b[1m\x1b[31m${errStr}`);
+}
+printErrStyle("hi");
 const args = process.argv.slice(2);
 const filePath = args[0];
 //test for a -o flag and set the output path to the command line argument directly adjacant to it otherwise default to style.css
